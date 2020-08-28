e460e5343d3cd63c70ad8813f8828a41faf38a11
Quardiz <quardiz@protonmail.com>
2020-08-28 21:50:00 +0200

    Updates

---
+++ precache-manifest.5a69d2d61bcfbacad4d828e5a8dc371d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe0f045e16d687fa673050faa6d73fa6",
+    "url": "/SunspotHistoryChart/index.html"
+  },
+  {
+    "revision": "3df72c9d1b20fd8075fd",
+    "url": "/SunspotHistoryChart/static/css/main.fbf60758.chunk.css"
+  },
+  {
+    "revision": "67090d1d6f4aa22c872b",
+    "url": "/SunspotHistoryChart/static/js/2.4c5cb92f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SunspotHistoryChart/static/js/2.4c5cb92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df72c9d1b20fd8075fd",
+    "url": "/SunspotHistoryChart/static/js/main.1580ceba.chunk.js"
+  },
+  {
+    "revision": "df2a858be96899201f63",
+    "url": "/SunspotHistoryChart/static/js/runtime-main.69a2fd34.js"
+  },
+  {
+    "revision": "03959240400375e2ec63d2fe7f3136dd",
+    "url": "/SunspotHistoryChart/static/media/sun.03959240.jpg"
+  }
+]);
